b55cac1b51685385a35abb88e6d8934997dcffc3
Usama Sheikh <usamasheikhoffical786@gmail.com>
2024-05-26 16:49:32 +0500

    Push my todo list app to github

---
+++ main.ts
@@ -0,0 +1,32 @@
+import { log } from 'console';
+import  inquirer from 'inquirer';
+
+let todos =[];
+
+let condition = true;
+
+while(condition)
+    {
+
+     let todosItem = await inquirer.prompt(
+        [
+            {
+                name:'todo',
+                type:'input',
+                message:'What you want to add your Todos?'
+            },
+
+            {
+                name:'moreTodos',
+                type:'confirm',
+                message:'Want to more your add Todos'
+            }
+
+        ]
+     )    
+
+     todos.push(todosItem.todo);
+     condition =todosItem.moreTodos;
+     console.log(todos);
+
+}
